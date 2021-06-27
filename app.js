6790d754140fb8795b22e1a935bf521b4f9eae9f
sukeroku-i <suke_imu@yahoo.co.jp>
2021-06-27 17:41:10 +0900

    テスト練習だす

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const num = process.argv[2] || 0;
+function kaijo(num) {
 
-'引数の判断'
+let result;
+
+'引数画0かそれ以外で判断'
 let sum;
 if  (num == 0){
     sum = 0;
@@ -10,9 +12,21 @@ if  (num == 0){
     sum = 1;
 }
 
+'階乗の計算'
 for(let i = 1; i <= num; i++){
     sum = sum * i;
 }
 
-console.log(`${num}の階乗は${sum}です。`);
+'計算結果の返却'
+result = (`${num}の階乗は${sum}です。`);
+return result;
+
+};
+
+'コンソールから数を取得し、階乗の関数を呼び出し'
+const num = process.argv[2] || 0;
+const returnmoji = kaijo(num);
+console.log(returnmoji);
+
+
 
